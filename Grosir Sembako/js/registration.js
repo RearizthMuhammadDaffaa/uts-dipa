49b9049af651efd77d85b0d5d077f0f1d1979f01
Rearizth <116627774+RearizthMuhammadDaffaa@users.noreply.github.com>
2023-12-28 15:23:21 +0700

    Delete Grosir Sembako directory

--- Grosir Sembako/js/registration.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator){
-  navigator.serviceWorker.register("sw.js").then(reg => {
-    console.log("SW Registered",reg);
-    
-  }).catch(err =>{
-    console.log("SW Registration fail");
-    console.log(err);
-  })
-}
