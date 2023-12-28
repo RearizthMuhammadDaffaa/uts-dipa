614cb93ad6cce82d28b6e9c8a40dcff40aa2ce5f
Rearizth <116627774+RearizthMuhammadDaffaa@users.noreply.github.com>
2023-12-28 15:26:33 +0700

    Add files via upload

---
+++ Grosir Sembako/utils.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    Produk:"beras",
+    harga:60000,
+    kategori:"makanan"
+  },
+  {
+    Produk:"telur",
+    harga:50000,
+    kategori:"makanan"
+  },
+  {
+    Produk:"Teh Pucuk",
+    harga:6000,
+    kategori:"minuman"
+  }
+]
